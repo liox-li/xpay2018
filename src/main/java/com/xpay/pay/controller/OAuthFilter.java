8a6e3199b53b6b7f39c85ba3c2e87c231fc675bf
Zaiping Ma <zaiping.ma@blackboard.com>
2017-04-01 11:04:50 +0800

    auth pass

--- src/main/java/com/xpay/pay/controller/OAuthFilter.java
+++ src/main/java/com/xpay/pay/controller/OAuthFilter.java
@@ -10,17 +10,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.oauth.OAuth;
+import net.oauth.OAuthAccessor;
+import net.oauth.OAuthConsumer;
+import net.oauth.OAuthMessage;
+import net.oauth.OAuthServiceProvider;
+import net.oauth.SimpleOAuthValidator;
+import net.oauth.server.OAuthServlet;
+import net.oauth.signature.RSA_SHA1;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.core.util.KeyValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.xpay.pay.ApplicationConstants;
+import com.xpay.pay.exception.ApplicationException;
 import com.xpay.pay.exception.AuthException;
 import com.xpay.pay.model.App;
+import com.xpay.pay.rest.contract.BaseResponse;
 import com.xpay.pay.service.AppService;
-
+import com.xpay.pay.util.JsonUtils;
 
 public class OAuthFilter implements Filter {
 	@Autowired
@@ -29,45 +42,84 @@ public class OAuthFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
-	            filterConfig.getServletContext());
+				filterConfig.getServletContext());
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest)request;
-		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
 		String oauth = httpRequest.getHeader(ApplicationConstants.HEADER_OAUTH);
-		App app = doAuth(oauth);
-		if(app!=null) {
-			request.setAttribute(ApplicationConstants.ATTRIBUTE_APP, app);
-			chain.doFilter(request, response);
-		} else {
-			throw new AuthException("-1", "Unauthorized");
+		try {
+			App app = checkApp(oauth);
+			if (app != null) {
+				request.setAttribute(ApplicationConstants.ATTRIBUTE_APP, app);
+				checkOAuth(httpRequest, app);
+				chain.doFilter(request, response);
+			} else {
+				throw new AuthException("401", "Unauthorized, please check your key/secret");
+			}	
+		} catch (ApplicationException e) {
+			printErrorResponse(response, e);
 		}
+	}
+
+	private static final SimpleOAuthValidator validator=new SimpleOAuthValidator();
+	private void checkOAuth(HttpServletRequest httpRequest, App app) {
+		OAuthMessage message=OAuthServlet.getMessage(httpRequest,null);
+		OAuthConsumer consumer=new OAuthConsumer(null, app.getKey(), app.getSecret(), null);
+		consumer.setProperty(OAuth.OAUTH_SIGNATURE_METHOD, OAuth.HMAC_SHA1);
+		OAuthAccessor accessor=new OAuthAccessor(consumer);
 		
+		try {
+			validator.validateMessage(message,accessor);
+		} catch (Exception e) {
+			throw new AuthException("401", e.getMessage());
+		}
 	}
 
 	@Override
 	public void destroy() {
 	}
-	
+
 	private static final String OAUTH_PREFIX = "OAuth ";
 	private static final String CONSUMER_KEY = "oauth_consumer_key";
-	private App doAuth(String oauth) {
-		if(!StringUtils.startsWith(oauth, OAUTH_PREFIX)) {
+
+	private App checkApp(String oauth) {
+		if (!StringUtils.startsWith(oauth, OAUTH_PREFIX)) {
 			return null;
 		}
 		String[] keyPairs = oauth.substring(6).split(",");
-		KeyValuePair consumerKey = Arrays.stream(keyPairs).map(x -> {
-			String[] strArr = x.split("=");
-			return new KeyValuePair(strArr[0],strArr[1].substring(1, strArr[1].length()-1));
-		}).filter(k -> CONSUMER_KEY.equals(k.getKey())).findAny().orElse(null);
-		if(consumerKey == null || StringUtils.isBlank(consumerKey.getValue())) {
+		KeyValuePair consumerKey = Arrays
+				.stream(keyPairs)
+				.map(x -> {
+					String[] strArr = x.split("=");
+					return new KeyValuePair(strArr[0], strArr[1].substring(1,
+							strArr[1].length() - 1));
+				}).filter(k -> CONSUMER_KEY.equals(k.getKey())).findAny()
+				.orElse(null);
+		if (consumerKey == null || StringUtils.isBlank(consumerKey.getValue())) {
 			return null;
 		}
 		App app = appService.findByKey(consumerKey.getValue());
 		return app;
+	}
+
+	private void printErrorResponse(ServletResponse response,
+			ApplicationException exception) throws JsonProcessingException, IOException {
+		BaseResponse<?> errorResponse = new BaseResponse<Object>();
+		errorResponse.setStatus(exception.getStatus());
+		errorResponse
+				.setMessage("The request was rejected because authentication failed.");
+		errorResponse.setCode(exception.getCode());
+
+		if (response instanceof HttpServletResponse) {
+			((HttpServletResponse) response).setContentType("application/json");
+			((HttpServletResponse) response).setStatus(exception.getStatus());
+		}
+
+		response.getOutputStream().println(JsonUtils.toJson(errorResponse));
 	}
 
 }
