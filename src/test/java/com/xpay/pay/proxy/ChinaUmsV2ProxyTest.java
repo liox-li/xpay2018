f4802292166f778175ecf4c308588bb43e7ed29c
Zaiping Ma <zaiping.ma@blackboard.com>
2017-08-08 12:19:54 +0800

    china ums v2 config

--- src/test/java/com/xpay/pay/proxy/ChinaUmsV2ProxyTest.java
+++ src/test/java/com/xpay/pay/proxy/ChinaUmsV2ProxyTest.java
@@ -15,21 +15,26 @@ public class ChinaUmsV2ProxyTest extends BaseSpringJunitTest {
 	@Autowired 
 	private ChinaUmsV2Proxy proxy;
 	
-	//898319848160167
-	//898319848160168
-	//898319848160169
-	//898319848160170
-	//898319848160171
+	//898319848160268
+	//898319848160269
+	//898319848160270
+	//898319848160271
+	//898319848160272
+	//898319848160273
+	//898319848160274
+	//898319848160275
+	//898319848160276
+	//898319848160277
 	
 	//898340149000005
 	@Test
 	public void testUnifiedOrder() {
 		PaymentRequest request = new PaymentRequest();
-		request.setExtStoreId("898340149000005");
+		request.setExtStoreId("898319848160268");
 		request.setDeviceId("1213");
 		request.setPayChannel(PayChannel.WECHAT);
 		request.setTotalFee("0.01");
-		request.setOrderNo("3116201707311003354995996111");
+		request.setOrderNo("3116201708081218354995996111");
 		request.setReturnUrl("http://www.baidu.com");
 		request.setNotifyUrl("http://106.14.47.193/xpay/notify/chinaumsv2");
 		request.setSubject("测试商品");
