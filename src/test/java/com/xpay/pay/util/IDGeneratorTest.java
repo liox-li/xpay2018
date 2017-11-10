9b2331694f265313f4be187818009e9e3866b45c
Zaiping Ma <zaiping.ma@blackboard.com>
2017-11-10 14:16:32 +0800

    test

--- src/test/java/com/xpay/pay/util/IDGeneratorTest.java
+++ src/test/java/com/xpay/pay/util/IDGeneratorTest.java
@@ -32,8 +32,8 @@ public class IDGeneratorTest {
 	}
 	
 	@Test
-	public void testStoreChannels() throws FileNotFoundException, IOException {
-		String sql = "insert into bill_store_channel (ext_store_id, ext_store_name, payment_gateway, bill_type) values (%ext_store_id%, %ext_store_name%, %payment_gateway%, 'T1');";
+	public void testNewStoreChannels() throws FileNotFoundException, IOException {
+		String sql = "insert into bill_store_channel (ext_store_id, ext_store_name, payment_gateway, bill_type) values ('%ext_store_id%', '%ext_store_name%', '%payment_gateway%', 'T1');";
 		String extStoreName = "千汇万兴";
 		String paymenGateway = "CHINAUMSH5";
 		
@@ -41,10 +41,28 @@ public class IDGeneratorTest {
 		List<String> lines = IOUtils.readLines(new FileInputStream(filePath));
 		
 		for(String line : lines) {
-			String replacedSql = sql.replace("%ext_store_id%", "'"+line+"'")
-			.replace("%ext_store_name%", "'"+extStoreName+"'")
-			.replace("%payment_gateway%", "'"+paymenGateway+"'");
+			String replacedSql = sql.replace("%ext_store_id%", line)
+			.replace("%ext_store_name%", extStoreName)
+			.replace("%payment_gateway%", paymenGateway);
 			System.out.println(replacedSql);
 		}
+	}
+	
+	@Test
+	public void testChangeStoreChannels() throws FileNotFoundException, IOException {
+		String sql = "update bill_store set channels='%channels%' where code='%code%';";
+		String code = "T20170801143221368";
+		long startChannelId = 111;
+		long endChannelId =120;
+		StringBuilder sb = new StringBuilder();
+		for(long l=startChannelId; l<=endChannelId; l++) {
+			sb.append(l);
+			sb.append(",");
+		}
+		sb.setLength(sb.length()-1);
+		
+		String replacedSql = sql.replace("%channels%", sb.toString())
+				.replace("%code%", code);
+		System.out.println(replacedSql);
 	}
 }
