96c20cee4d1cedb5db663800e0d04c63c54051ec
Zaiping Ma <zaiping.ma@blackboard.com>
2017-11-27 11:24:14 +0800

    BAIL AGENT

--- src/main/java/com/xpay/pay/service/PaymentService.java
+++ src/main/java/com/xpay/pay/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.xpay.pay.service;
 
 import static com.xpay.pay.proxy.IPaymentProxy.NO_RESPONSE;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,7 +35,8 @@ public class PaymentService {
 	private StoreService storeService;
 	@Autowired
 	private RiskCheckService riskCheckService;
-
+	private static final long DEFAULT_BAIL_ID = 10L;
+	
 	public Order createOrder(App app, String orderNo, Store store, PayChannel channel,
 			String deviceId, String ip, String totalFee, String orderTime,
 			String sellerOrderNo, String attach, String notifyUrl,String returnUrl,
@@ -47,9 +49,13 @@ public class PaymentService {
 		} else {
 			boolean isNextBailPay = store.isNextBailPay(CommonUtils.toFloat(totalFee));
 			if(isNextBailPay) {
-				storeChannel = orderService.findUnusedChannel(store.getBailChannels(), orderNo);
+				if(CollectionUtils.isEmpty(store.getBailChannels())) {
+					storeChannel = orderService.findUnusedChannelByAgent(DEFAULT_BAIL_ID, orderNo);
+				} else {
+					storeChannel = orderService.findUnusedChannel(store.getBailChannels(), orderNo);
+				}
 			}
-			storeChannel = storeChannel==null? orderService.findUnusedChannel(store, orderNo):storeChannel;
+			storeChannel = storeChannel==null? orderService.findUnusedChannelByStore(store, orderNo):storeChannel;
 
 			
 		}
