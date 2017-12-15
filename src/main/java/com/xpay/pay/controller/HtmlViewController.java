139ea9ebf88eee457bd4943f89841b0543c49e3c
孙健 <jian.sun.arch@dianrong.com>
2017-12-15 18:17:05 +0800

    集成IPS 快捷 和 扫码 支付

--- src/main/java/com/xpay/pay/controller/HtmlViewController.java
+++ src/main/java/com/xpay/pay/controller/HtmlViewController.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -33,7 +34,7 @@ public class HtmlViewController {
   @Autowired
   private PaymentService paymentService;
 
-  @RequestMapping("/{orderNo}")
+  @RequestMapping(value = "/{orderNo}", method = RequestMethod.GET)
   public ModelAndView pay(@PathVariable("orderNo") String orderNo) {
     logger.info("h5pay :" + orderNo);
     Order order = orderService.findActiveByOrderNo(orderNo);
