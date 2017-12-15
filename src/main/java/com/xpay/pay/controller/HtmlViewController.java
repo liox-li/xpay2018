adb91f79dc754d88c6dcc48ebdbd4aae274b5233
孙健 <jian.sun.arch@dianrong.com>
2017-12-15 18:10:08 +0800

    集成IPS 快捷 和 扫码 支付

--- src/main/java/com/xpay/pay/controller/HtmlViewController.java
+++ src/main/java/com/xpay/pay/controller/HtmlViewController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,7 +19,6 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by sunjian on Date: 2017/12/15 下午2:12
  * Description:
  */
-@Controller
 @RequestMapping("/h5pay")
 public class HtmlViewController {
 
