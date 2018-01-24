fcebf8fcf9d7465e6e76bdd6992ea1279cb25193
zpingma <zpingma@gmail.com>
2018-01-24 11:15:56 +0800

    add notes in store goods

--- src/main/java/com/xpay/pay/model/StoreGoods.java
+++ src/main/java/com/xpay/pay/model/StoreGoods.java
@@ -1,8 +1,13 @@
 package com.xpay.pay.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import com.xpay.pay.util.JsonUtils;
 
 public class StoreGoods {
 	private Long id;
@@ -13,6 +18,8 @@ public class StoreGoods {
 	private String desc;
 	private Float amount;
 	private String extQrCode;
+	private String extGoods;
+	private List<ExtGoods> extGoodsList;
 	private Date updateDate;
 	
 	public Long getId() {
@@ -71,5 +78,54 @@ public class StoreGoods {
 	}
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
+	}
+	
+	public List<ExtGoods> getExtGoodsList() {
+		if(CollectionUtils.isEmpty(this.extGoodsList)) {
+			if(StringUtils.isNotBlank(extGoods)) {
+				extGoodsList = JsonUtils.fromJsonArray(extGoods, ExtGoods.class);
+			} else if(StringUtils.isNotBlank(extQrCode)) {
+				extGoodsList = new ArrayList<ExtGoods>();
+				String[] goodsArr = this.getExtQrCodes();
+				for(int i=0;i<goodsArr.length;i++) {
+					ExtGoods goods = new ExtGoods();
+					goods.setExtQrCode(goodsArr[i]);
+					goods.setNote(String.valueOf(i+1));
+					extGoodsList.add(goods);
+				}
+			}
+		}
+		
+		return extGoodsList;
+	}
+	public String getExtGoods() {
+		if(StringUtils.isBlank(extGoods)) {
+			List<ExtGoods> extGoodsList = this.getExtGoodsList();
+			this.extGoods = JsonUtils.toJson(extGoodsList);
+		}
+		return extGoods;
+	}
+	public void setExtGoods(String extGoods) {
+		this.extGoods = extGoods;
+	}
+	public void setExtGoodsList(List<ExtGoods> extGoodsList) {
+		this.extGoodsList = extGoodsList;
+	}
+
+	public static class ExtGoods {
+		private String extQrCode;
+		private String note;
+		public String getExtQrCode() {
+			return extQrCode;
+		}
+		public void setExtQrCode(String extQrCode) {
+			this.extQrCode = extQrCode;
+		}
+		public String getNote() {
+			return note;
+		}
+		public void setNote(String note) {
+			this.note = note;
+		}
 	}
 }
